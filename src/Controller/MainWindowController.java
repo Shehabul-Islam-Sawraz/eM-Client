8b5fcb9d45027724ac1ee9f62b3e409450c89fde
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-07-27 01:26:19 +0600

    Login.fxml modified, fxml file for main window added

---
+++ src/Controller/MainWindowController.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.web.WebView;
+
+public class MainWindowController {
+
+    @FXML
+    private TreeView<?> emailsTreeView;
+
+    @FXML
+    private TableView<?> emailsTableView;
+
+    @FXML
+    private WebView emailsWebView;
+
+    @FXML
+    void optionsMenuPressed() {
+
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module eM.Client {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires javafx.web;
 
     opens sample;
     opens Controller to javafx.fxml;
